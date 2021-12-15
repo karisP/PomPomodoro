89d2d8315a52f1360687e3f142dfded5cff1f5b9
Karissa Phillips <phillips.karissa00@gmail.com>
2021-12-15 14:59:57 -0500

    handles initial click and edge case logic

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -2,10 +2,12 @@ import React from 'react';
 import useInterval from '../hooks/useInterval';
 
 const Timer = () => {
-    const [minute, setMinute] = React.useState(25)
+    const initialMinutes = 25;
+    const [minute, setMinute] = React.useState(initialMinutes);
     const [second, setSecond] = React.useState(60);
     const [minuteDelay, setMinuteDelay] = React.useState(null);
     const [secondDelay, setSecondDelay] = React.useState(null);
+    const [clickCount, setClickCount] = React.useState(0);
     useInterval(() => countDownSeconds(), secondDelay);
     useInterval(() => countDownMinutes(), minuteDelay);
 
@@ -13,7 +15,7 @@ const Timer = () => {
         if (minute > 0){
             return setMinute(minute - 1);
         } else {
-            stopTimer();
+            setMinuteDelay(null);
         };
     };
 
@@ -21,11 +23,21 @@ const Timer = () => {
         if (second > 0){
             setSecond(second - 1);
         } else {
-            setSecond(59);
+            if (minute === 0 && second === 0){
+                stopTimer();
+            } else {
+                setSecond(59);
+            };
         };
     };
 
     const startTimer = () => {
+        //handles first click immediately subtracting minute
+        if (clickCount === 0){
+            setMinute(minute - 1);
+            setSecond(second - 1);
+            setClickCount(clickCount + 1);
+        }
         setSecondDelay(1000);
         setMinuteDelay(60000);
     };
@@ -33,6 +45,13 @@ const Timer = () => {
     const stopTimer = () => {
         setSecondDelay(null);
         setMinuteDelay(null);
+    };
+
+    const clearTimer = () => {
+        stopTimer();
+        setMinute(initialMinutes);
+        setSecond(60);
+        setClickCount(0);
     };
 
     let displayTime = (num) => {
@@ -46,6 +65,7 @@ const Timer = () => {
             <div>{displayTime(minute)} : {displayTime(second)}</div>
             <button onClick={startTimer}>Start Timer</button>
             <button onClick={stopTimer}>Stop Timer</button>
+            <button onClick={clearTimer}>Clear Timer</button>
         </>
     );
 };
